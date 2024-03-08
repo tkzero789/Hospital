440c1a3f889ab8cb3a0af1262222e6e40da29b8b
AnhKiet112k1 <kiet.nguyen112k1@hcmut.edu.vn>
2024-03-08 11:59:55 +0700

    auth part 1.2.1

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const session = require("express-session");
+const session = require("cookie-session");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
