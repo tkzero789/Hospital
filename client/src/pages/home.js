5af0b11f398ed10e733aafd4158afbff3f70b897
wamu <101846564+tkzero789@users.noreply.github.com>
2024-03-27 21:42:10 -0400

    Carousel changes

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -102,58 +102,38 @@ export default function Home() {
         {/* CAROUSEL */}
         <div
           id="carouselExampleInterval"
-          class="carousel slide d-none d-lg-block d-xl-block"
+          class="carousel slide"
           data-bs-ride="carousel"
         >
-          <div className="hero-section">
-            <div class="carousel-inner">
-              <div class="carousel-item active" data-bs-interval="5000">
-                <img
-                  className="car-img d-block w-100"
-                  src={HI1}
-                  alt="Background 1"
-                />
-              </div>
-              <div class="carousel-item" data-bs-interval="5000">
-                <img
-                  className="car-img d-block w-100"
-                  src={HI2}
-                  alt="Background 2"
-                />
-              </div>
-              <div class="carousel-item" data-bs-interval="5000">
-                <img
-                  className="car-img d-block w-100"
-                  src={HI3}
-                  alt="Background 3"
-                />
-              </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="5000">
+              <img className="car-img" src={HI1} alt="Background 1" />
             </div>
-            <button
-              class="carousel-control-prev"
-              type="button"
-              data-bs-target="#carouselExampleInterval"
-              data-bs-slide="prev"
-            >
-              <span
-                class="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button
-              class="carousel-control-next"
-              type="button"
-              data-bs-target="#carouselExampleInterval"
-              data-bs-slide="next"
-            >
-              <span
-                class="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="visually-hidden">Next</span>
-            </button>
+            <div class="carousel-item" data-bs-interval="5000">
+              <img className="car-img" src={HI2} alt="Background 2" />
+            </div>
+            <div class="carousel-item">
+              <img className="car-img" src={HI3} alt="Background 3" />
+            </div>
           </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleInterval"
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleInterval"
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
 
         <Options />
