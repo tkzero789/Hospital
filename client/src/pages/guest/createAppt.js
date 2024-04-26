13c721a721e80bcc5457facc98395d4fc85c722a
wamu <101846564+tkzero789@users.noreply.github.com>
2024-04-26 11:41:22 -0400

    quick fixed createAppt - ApptForm

--- client/src/pages/guest/createAppt.js
+++ client/src/pages/guest/createAppt.js
@@ -24,54 +24,37 @@ export default function CreateAppt() {
   const navigate = useNavigate();
 
   async function confirmSetAppt(e) {
-    const inputFullName = document.getElementById("inputFullName");
-    const inputPhoneNumber = document.getElementById("inputPhoneNumber");
-    const inputEmail = document.getElementById("inputEmail");
-    if (!inputFullName.checkValidity()) {
-      alert("Thiếu Họ và tên");
-    } else if (appt.phoneNumber === "") {
-      alert("Số điện thoại không hợp lệ");
-    } else if (!inputPhoneNumber.checkValidity()) {
-      alert("Số điện thoại không hợp lệ");
-    } else if (!inputEmail.checkValidity()) {
-      alert("Email không hợp lệ");
-    } else if (appt.gender === "") {
-      alert("Vui lòng chọn giới tính");
-    } else if (appt.need === "") {
-      alert("Vui lòng chọn Nhu cầu khám");
-    } else {
-      e.preventDefault();
-      const updatedAppt = {
-        ...appt,
-        createdAt: new Date().toLocaleDateString("vi-VN", {
-          year: "numeric",
-          month: "2-digit",
-          day: "2-digit",
-        }),
-      };
+    e.preventDefault();
+    const updatedAppt = {
+      ...appt,
+      createdAt: new Date().toLocaleDateString("vi-VN", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      }),
+    };
 
-      axios
-        .post("http://localhost:5000/appointment/add", updatedAppt)
-        .then((res) => {
-          console.log("Appointment set", res.data);
-          setAppt({
-            fullName: "",
-            phoneNumber: "",
-            email: "",
-            dob: "",
-            gender: "",
-            need: "",
-            date: "",
-            reason: "",
-            createdAt: null,
-          });
-          navigate("/");
-        })
-        .catch((err) => {
-          const message = `Có lỗi xảy ra: ${err}`;
-          window.alert(message);
+    axios
+      .post("http://localhost:5000/appointment/add", updatedAppt)
+      .then((res) => {
+        console.log("Appointment set", res.data);
+        setAppt({
+          fullName: "",
+          phoneNumber: "",
+          email: "",
+          dob: "",
+          gender: "",
+          need: "",
+          date: "",
+          reason: "",
+          createdAt: null,
         });
-    }
+        navigate("/");
+      })
+      .catch((err) => {
+        const message = `Có lỗi xảy ra: ${err}`;
+        window.alert(message);
+      });
   }
 
   const closeModal = () => {
@@ -86,15 +69,10 @@ export default function CreateAppt() {
         showModal={showModal}
         setShowModal={setShowModal}
         editMode={true}
+        closeModal={closeModal}
+        confirmSetAppt={confirmSetAppt}
       />
-      <div className="appt-modal-btn">
-        <button type="button" onClick={closeModal}>
-          Quay lại
-        </button>
-        <button type="button" onClick={confirmSetAppt}>
-          Đăng ký khám
-        </button>{" "}
-      </div>
+
       <Footer />
     </div>
   );
