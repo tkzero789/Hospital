a704b58055fb114640e06eb0230d7ceb9714f3ea
wamu <101846564+tkzero789@users.noreply.github.com>
2024-06-24 18:07:07 -0400

    fix: scss file name case sensitive

--- client/src/components/HomePage/SupportUs/SupportUs.jsx
+++ client/src/components/HomePage/SupportUs/SupportUs.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "components/HomePage/SupportUs/Supportus.scss";
+import "components/HomePage/SupportUs/SupportUs.scss";
 
 const SupportUs = () => {
   return (
