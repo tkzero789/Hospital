0f11b93e247a7eb30a6a46d4444a82c4e2594744
wamu <101846564+tkzero789@users.noreply.github.com>
2024-06-24 17:25:59 -0400

    fix: scss file name case sensitive

--- client/src/components/HomePage/ConvenienceCenter/ConvenienceCenter.jsx
+++ client/src/components/HomePage/ConvenienceCenter/ConvenienceCenter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "components/HomePage/ConvenienceCenter/Convcenter.scss";
+import "components/HomePage/ConvenienceCenter/ConvCenter.scss";
 
 export default function ConvenienceCenter() {
   return (
